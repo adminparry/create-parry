58f09a2e660e0e74718cdf4792815d7281e6f8c0
adminparry <1170015014@qq.com>
2025-06-15 18:34:09 +0800

    fix:  auth filter

--- template-springboot/src/main/java/com/example/demo/auth/filter/JwtAuthenticationFilter.java
+++ template-springboot/src/main/java/com/example/demo/auth/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,8 @@
 package com.example.demo.auth.filter;
 
 import com.example.demo.auth.config.SsoConfig;
-import com.example.demo.auth.config.UserContextHolder;
 import com.example.demo.auth.exception.SsoException;
-import com.example.demo.utils.RedisUtil;
-import com.example.demo.utils.SsoUtil;
+import com.example.demo.utils.*;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
@@ -18,23 +16,27 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
-@Component
-@Order(1)
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
+
     private SsoUtil ssoUtil;
+    static volatile JsonUtil jsonUtil;
 
 
-    @Autowired
     private SsoConfig ssoProperties;
 
-
+    public JwtAuthenticationFilter(SsoUtil ssoUtil, SsoConfig ssoProperties) {
+        this.ssoUtil = ssoUtil;
+        this.ssoProperties = ssoProperties;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        log.info("\r\n========={}=============", TraceUtil.all());
         if("options".equals(request.getMethod())){
             filterChain.doFilter(request, response);
         }
@@ -44,20 +46,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         };
 
         String token = ssoUtil.getTokenFromRequest(request);
-
+        response.setContentType("application/json;charset=UTF-8");
         try {
             Claims claims = ssoUtil.validateAndParseToken(token);
-
             // 将用户信息存入请求属性
             request.setAttribute("userId", claims.getSubject());
             request.setAttribute("username", claims.get("username"));
-
-//            UserContextHolder.set();
-
             filterChain.doFilter(request, response);
         } catch (JwtException e) {
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "无效或过期的令牌");
-            throw new SsoException("forbidden");
+            log.info("{}", e.getMessage());
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+
+            StreamUtil.writeUnauthorized(e.getMessage(), response.getWriter());
+
+
         }
     }
 }
